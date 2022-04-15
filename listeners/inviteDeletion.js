7aada1f3639e25def5ee88a28962cc99cd06487b
Marian <elias-marian@web.de>
2022-04-15 22:47:49 +0200

    :lock: Added owner bypass

--- listeners/inviteDeletion.js
+++ listeners/inviteDeletion.js
@@ -1,5 +1,8 @@
+const config = require("../config.json")
+
 module.exports = {
     messageCreate: async (message) => {
+        if (message.member.roles.cache.map(role => role.id).includes(config.roles.owner)) return
         const regex = /(http(s)?:\/\/)?discord\.gg\/.+/g
         if (regex.test(message.content)) return message.delete()
     }
